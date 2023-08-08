e22c2556f258d9344141044d454fa5843b3ccc23
Dhruv Mehta <dhruv.mehta@convergesolution.com>
2023-08-08 23:34:34 +0530

    Final

---
+++ Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Planet
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<Moon> Moons { get; set; }
+        public float AverageMoonGravity
+        {
+            get;
+            set;
+        } = 0.0f;
+
+        public Planet(PlanetDto planetDto)
+        {
+            
+            Id = planetDto.Id;
+            SemiMajorAxis = planetDto.SemiMajorAxis;
+            //Moons = new Collection<Moon>();
+            Moons = planetDto.Moons != null ? planetDto.Moons.Select(moonDto => new Moon(moonDto)).ToList() : new List<Moon>();
+            AverageMoonGravity = (planetDto.Moons != null)?planetDto.Moons.Average(m => m.Gravity):0;
+            //if (planetDto.Moons != null)
+            //{
+            //    foreach (MoonDto moonDto in planetDto.Moons)
+            //    {
+            //        Moons.Add(new Moon(moonDto));
+            //    }
+            //}
+        }
+
+        public Boolean HasMoons()
+        {
+            return (Moons != null && Moons.Count > 0);
+        }
+    }
+}
---
+++ Test-Taste-Console-Application/Domain/Services/PlanetService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using Newtonsoft.Json;
+using Test_Taste_Console_Application.Constants;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+using Test_Taste_Console_Application.Domain.DataTransferObjects.JsonObjects;
+using Test_Taste_Console_Application.Domain.Objects;
+using Test_Taste_Console_Application.Domain.Services.Interfaces;
+using Test_Taste_Console_Application.Utilities;
+
+namespace Test_Taste_Console_Application.Domain.Services
+{
+    /// <inheritdoc />
+    public class PlanetService : IPlanetService
+    {
+        private readonly HttpClientService _httpClientService;
+
+        public PlanetService(HttpClientService httpClientService)
+        {
+            _httpClientService = httpClientService;
+            Console.WriteLine("Loading...");
+        }
+       
+        public IEnumerable<Planet> GetAllPlanets()
+        {
+            var allPlanetsWithTheirMoons = new Collection<Planet>();
+
+            var response = _httpClientService.Client
+                .GetAsync(UriPath.GetAllPlanetsWithMoonsQueryParameters)
+                .Result;
+
+            //If the status code isn't 200-299, then the function returns an empty collection.
+            if (!response.IsSuccessStatusCode)
+            {
+                Logger.Instance.Warn($"{LoggerMessage.GetRequestFailed}{response.StatusCode}");
+                return allPlanetsWithTheirMoons;
+            }
+
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            //The JSON converter uses DTO's, that can be found in the DataTransferObjects folder, to deserialize the response content.
+            var results = JsonConvert.DeserializeObject<JsonResult<PlanetDto>>(content);
+
+            //The JSON converter can return a null object. 
+            if (results == null) return allPlanetsWithTheirMoons;
+
+            var moonResponse = _httpClientService.Client
+                           .GetAsync(UriPath.GetMoonByIdQueryParameters)
+                           .Result;
+            var moonContent = moonResponse.Content.ReadAsStringAsync().Result;
+            var moonData = JsonConvert.DeserializeObject<Dictionary<string, List<MoonDto>>>(moonContent);
+            var allMoons = moonData["bodies"];
+
+            //If the planet doesn't have any moons, then it isn't added to the collection.
+            foreach (var planet in results.Bodies)
+            {
+                if (planet.Moons != null)
+                {
+                    var newMoonsCollection = new Collection<MoonDto>();
+                    //foreach (var moon in planet.Moons)
+                    //{
+                    //    var moonResponse = _httpClientService.Client
+                    //        .GetAsync(UriPath.GetMoonByIdQueryParameters + moon.URLId)
+                    //        .Result;
+                    //    var moonContent = moonResponse.Content.ReadAsStringAsync().Result;
+                    //    newMoonsCollection.Add(JsonConvert.DeserializeObject<MoonDto>(moonContent));
+                    //}
+                    foreach (var moon in allMoons.Where(m => m.Planet != null && m.Planet.Contains(planet.Id)).ToList())
+                    {
+                        newMoonsCollection.Add(moon);
+                    }
+                    planet.Moons = newMoonsCollection;
+                }
+                allPlanetsWithTheirMoons.Add(new Planet(planet));
+            }
+
+            return allPlanetsWithTheirMoons;
+        }
+
+        private static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+            for (int i = 0; i < normalizedString.Length; i++)
+            {
+                char c = normalizedString[i];
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+    }
+}
